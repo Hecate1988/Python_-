58ee5d0bc61dff047dab1479a5fedcd0adc852b4
Hecate1988 <mesheryakova.mosk.syd@gmail.com>
2023-01-26 15:00:05 +0200

    first commit

---
+++ Условные операторы.py
@@ -0,0 +1,26 @@
+if 3>2:
+    print("It's True")
+
+hungry = True
+if hungry:
+    print("Feed me!")
+
+a = False
+if a:
+    print("Ok!")
+else:
+    print("Not Ok!!!!")
+
+b = True
+if b:
+    print("Ok!")
+else:
+    print("Not Ok!!!!")
+
+flat = "big"
+if flat == 'red':
+    print('Red flat')
+elif flat == 'big':
+    print('Big flat')
+else:
+    print('I do not have a flat')
