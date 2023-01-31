824385b2104ea9f39b498512c7ecf781ab26eefc
Hecate1988 <mesheryakova.mosk.syd@gmail.com>
2023-01-31 19:53:36 +0200

    second commit

--- lesson30.01.2023_dz9.py
+++ lesson30.01.2023_dz9.py
@@ -1,5 +1,5 @@
 from random import randint
-a = [randint(0,10) for _ in range(randint(3,10))]
-print(a)
-print ([a[0]] + [a[2]] + [a[-2]])
+lst = [randint(0,10) for lst in range(randint(3,10))]
+print(lst)
+print ([lst[0]] + [lst[2]] + [lst[-2]])
 
